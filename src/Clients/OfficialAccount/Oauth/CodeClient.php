f8169ea7e72a2fd4e0a29b578970ee37bd10124e
TenFish <474260946@qq.com>
2020-06-18 10:18:42 +0800

    bug

--- src/Clients/OfficialAccount/Oauth/CodeClient.php
+++ src/Clients/OfficialAccount/Oauth/CodeClient.php
@@ -25,6 +25,6 @@ final class CodeClient extends CoreClient
     public function getUrl()
     {
         $state = sha1(uniqid(mt_rand(1, 1000000), true));
-        return sprintf('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s#wechat_redirect', $this->config['appId'], $this->config['redirectUri'], $this->config['scope'], $state);
+        return sprintf('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s#wechat_redirect', $this->config['appId'], urlencode($this->config['redirectUri']), $this->config['scope'], $state);
     }
 }
